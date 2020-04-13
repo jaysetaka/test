e22daedca0f480e859721614ca5e59107e6ad0ab
jabucwm <jabu@clockworkmedia.co.za>
2020-04-13 13:23:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Test App To Deploy</h1>`);
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
